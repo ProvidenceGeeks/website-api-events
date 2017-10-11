7e6adb676f9c47e9be94dc528c62fcfcfb46f6fb
Mrunalini_Diwale <mdiwale@kenzan.com>
2017-10-11 16:22:18 -0400

    Change artifact and group id

--- src/main/java/com/providencegeeks/TestApplication.java
+++ src/main/java/com/providencegeeks/TestApplication.java
@@ -1,4 +1,4 @@
-package com.example.test;
+package com.providencegeeks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/providencegeeks/websiteapipostsTests.java
+++ src/test/java/com/providencegeeks/websiteapipostsTests.java
@@ -1,4 +1,4 @@
-package com.example.test;
+package com.providencegeeks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestApplicationTests {
+public class websiteapipostsTests {
 
 	@Test
 	public void contextLoads() {
